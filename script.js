698d4fbb1f0ea921d1338c22e67442634899abd8
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-07-07 16:32:35 -0300

    adicionando função de calcular valor

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-let valorFinal = 3400;
-valorFinal = Number(valorFinal)
+let valorFinalEncantos = 3600;
+let valorFinalSuper = 4600;
 
 
-document.getElementById("valor-pacote-encantos").innerHTML = "Valor R$ "+ valorFinal;
+document.getElementById("valor-pacote-encantos").innerHTML = "Valor R$ "+ valorFinalEncantos;
+document.getElementById("valor-pacote-superencantos").innerHTML = "Valor R$ "+ valorFinalSuper;
 
 
 document.getElementById("num-convidados").onclick = function(){
     let convidados = document.getElementById("num-convidados").value;
     convidados = Number(convidados);
 
-    if(convidados=>50){
+    if((convidados>=30)&&(convidados <= 100)){
         let diferença = convidados - 50;
         diferença *= 40;
-        valorFinal = 3400;
-        valorFinal += diferença;
-        document.getElementById("valor-pacote-encantos").innerHTML = "Valor R$ "+ valorFinal;
+        valorFinalEncantos = 3600;
+        valorFinalEncantos += diferença;
+        document.getElementById("valor-pacote-encantos").innerHTML = "Valor R$ "+ valorFinalEncantos;
+        console.log( "valorFinal="+ valorFinalEncantos+ "   "+ "diferença= "+diferença+"   ");
+    }else{
+        document.getElementById("valor-pacote-encantos").innerHTML = "Nossa capacitade é de<br>30 até 100 convidados";
+    }
+    
+}
+
+document.getElementById("num-convidados1").onclick = function(){
+    let convidados = document.getElementById("num-convidados1").value;
+    convidados = Number(convidados);
+
+    if((convidados>=30)&&(convidados <= 100)){
+        let diferença = convidados - 50;
+        diferença *= 45;
+        valorFinalSuper = 4600;
+        valorFinalSuper += diferença;
+        document.getElementById("valor-pacote-superencantos").innerHTML = "Valor R$ "+ valorFinalSuper;
+    }else{
+        document.getElementById("valor-pacote-superencantos").innerHTML = "Nossa capacitade é de<br>30 até 100 convidados";
     }
 }
